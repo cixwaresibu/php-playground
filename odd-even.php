44d9695f84995da2d4c54660af8a3b2447591155
Sibu Dhital <cixwaresibu@gmail.com>
2020-02-26 00:21:48 +0545

    checking of either the given number is odd or even

---
+++ odd-even.php
@@ -0,0 +1,12 @@
+<?php 
+    $testNum = 0;
+
+    if($testNum%2==0)
+    {
+        echo "$testNum is an even number <br/>";
+    }
+    else 
+    {
+        echo "$testNum is an odd number <br/>";
+    }
+?>
