d1ad340d218c1d418865e7c0e2c626de0a47ce64
Sibu Dhital <cixwaresibu@gmail.com>
2020-02-26 08:37:41 +0545

    line breaking is done

--- arithmetic-operation.php
+++ arithmetic-operation.php
@@ -4,14 +4,14 @@
     $result;
     $result = $num1 + $num2;
     echo "Addition of $num1 and $num2 is $result"; 
-    echo "\n";
+    echo "<br/>";
     $result = $num1 - $num2;
     echo "Subtraction of $num1 and $num2 is $result";
-    echo "\n";
+    echo "<br/>";
     $result = $num1 * $num2;
     echo "Multiplication of $num1 and $num2 is $result";
-    echo "\n";
+    echo "<br/>";
     $result = $num1 / $num2;
     echo "Division of $num1 by $num2 is $result";
-    echo "\n";
+    
 ?>
