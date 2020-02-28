ae343d90ceb42c7e1bd3cde527a54d04d367b5d2
Sibu Dhital <cixwaresibu@gmail.com>
2020-02-28 07:08:19 +0545

    finding maximum number from an array without sorting using udf and without using udf

--- array-max.php
+++ array-max.php
@@ -8,4 +8,20 @@
         }
     }
     echo $max;
+    echo "<br/>";
+    echo "<br/>";
+    echo "<br/>";
+
+    function arrayMaxNumber($array){
+        $total = count($array);
+        $maximum = $array[0];
+        for ($i=0;$i<$total;$i++){
+            if($array[$i]>$maximum){
+                $maximum = $array[$i];
+            }
+        }
+        return $maximum;
+    }
+    
+    echo arrayMaxNumber([15, 20, 5, 4, 3, 20, 6, 51, 99, 80]);
 ?>
