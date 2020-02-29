6d3059ac0630788dd2daf23fc8754f351536b4b6
Sibu Dhital <cixwaresibu@gmail.com>
2020-02-29 20:53:02 +0545

    string concatination

---
+++ string/string-operation.php
@@ -0,0 +1,11 @@
+<?php
+    //string concatination
+    $str1 = 'hey';
+    $str2 = 'sibu';
+    echo $str = $str1." ".$str2;
+
+        echo "<br/>";
+    //concatination assignment
+    $str1 = 'hey';
+    echo $str1 .=" ".$str2;
+?>
