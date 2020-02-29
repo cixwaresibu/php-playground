8a79003d3e77d42cdffa8abd4a1996cfdaaa3499
Sibu Dhital <cixwaresibu@gmail.com>
2020-02-29 20:59:40 +0545

    substring extraction and position of string

---
+++ string/str-op.php
@@ -0,0 +1,11 @@
+<?php
+    //substring extraction 
+    $str = 'hello sibu don';
+    echo substr($str,6,4);
+    echo "<br>";
+    echo $str[11];
+    echo "<br>";
+
+    //position of a string
+    echo strpos("$str","sibu");
+?>
