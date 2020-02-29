a0c5fc5b4a96044ea604fe3155c4cf8a4ecdf36f
Sibu Dhital <cixwaresibu@gmail.com>
2020-02-29 20:52:37 +0545

    view modified

--- student-system.php
+++ student-system.php
@@ -1,8 +1,8 @@
 <?php
     $marks = [19, 90, 4, 29, 1, 23, 5, 88, 73, 32, 42, 28, 91, 8, 52, 76, 82, 63, 33, 20, 12, 9, 71, 53, 87, 69, 70, 41, 45, 2, 58, 61, 64, 18, 0, 7, 30, 86, 80, 75, 39, 56, 81, 51, 84, 83, 89, 50, 36, 77];
-    echo 'marks obtained by the student in annual exam is :'."<br/>";
+    // echo 'marks obtained by the student in annual exam is :'."<br/>";
     
-    print_r($marks);
+    // print_r($marks);
     echo "<br/>";
    $array1=[];
    $array2=[];
