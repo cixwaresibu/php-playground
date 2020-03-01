41293c5ff6cfe08f6c7a9db900eee71311f6d9fb
Sibu Dhital <cixwaresibu@gmail.com>
2020-03-01 13:00:06 +0545

    calculating string length with strlen

---
+++ string/string-length.php
@@ -0,0 +1,7 @@
+<?php
+    $str='hey bob! it\'s your hub';
+    $token = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, consectetur. Numquam corporis excepturi suscipit culpa cum dolor id libero doloribus, ipsam quis impedit maiores, sequi ea laborum illum modi debitis!';
+    echo strlen($str);
+    echo "<br>";
+    echo strlen($token);
+?>
