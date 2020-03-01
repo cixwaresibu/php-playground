73cb5626522814501fa4ad59e7a28eb03749e2f9
Sibu Dhital <cixwaresibu@gmail.com>
2020-03-01 12:30:46 +0545

    finding the position of given substring in given string at its first occurance

--- string/string-position.php
+++ string/string-position.php
@@ -1,5 +1,5 @@
 <?php
-    $str='hey it\'s me sakar';
+    $str='hey it\'s me sakar and it is very important';
     echo $str;
     echo "<br>";
     echo 'the given substring is at position ' . strpos($str,"it") .' in given string: '.'"' .$str. '"';
