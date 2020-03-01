c5012dd47e19fe2f7673d91b00a850725405d664
Sibu Dhital <cixwaresibu@gmail.com>
2020-03-01 13:04:26 +0545

    counting total words in a string with str_word_count

---
+++ string/string-word-count.php
@@ -0,0 +1,7 @@
+<?php
+    $lorem = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia libero provident, non explicabo aperiam dignissimos veritatis minus eius impedit numquam ex molestiae modi ipsam mollitia sequi dolore! Magnam corrupti fugiat, quo, mollitia odit vel quisquam accusantium inventore consectetur rerum iste, voluptates porro libero odio autem ipsum veritatis dicta cum eaque?';
+    $str = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse voluptates doloremque, perspiciatis aliquid beatae nobis consequuntur quibusdam maiores ipsam ad. Sint quibusdam dolores minima unde fugiat eaque tempore necessitatibus est? Sint quibusdam dolores minima unde fugiat eaque tempore necessitatibus est?';
+    echo str_word_count($lorem);
+    echo "<br>";
+    echo str_word_count($str);
+?>
