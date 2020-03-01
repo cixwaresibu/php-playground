04f9847842ed88acc09ffd7f84f0ec41122fdf2a
Sibu Dhital <cixwaresibu@gmail.com>
2020-03-01 15:31:53 +0545

    ltrim rtrim and trim example

---
+++ string/ltrim.php
@@ -0,0 +1,10 @@
+<?php
+    $str = 'hey there it\'s me sibu dhital hey there';
+    echo $str;
+    echo "<br>";
+    echo rtrim($str,"hey there");
+    echo "<br>";
+    echo ltrim($str,"hey there");
+    echo "<br>";
+    echo trim($str,"hey there");
+?>
