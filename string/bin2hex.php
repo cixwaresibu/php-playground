ce17449ea16c36cac9dfdbf69090580d9da6bf30
Sibu Dhital <cixwaresibu@gmail.com>
2020-03-01 14:38:40 +0545

    bin2hex and hex2bin conversion

---
+++ string/bin2hex.php
@@ -0,0 +1,10 @@
+<?php
+    $str = 'hey man! why are you so sad';
+    $name = '53494255';
+    $garbage = 'sibu';
+    echo bin2hex($str);
+    echo "<br>";
+    echo bin2hex($garbage);
+    echo "<br>";
+    echo hex2bin($name);
+?>
