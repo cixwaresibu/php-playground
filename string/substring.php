489c05d83ad6d293ae3ff4d09336c486c8faa48f
Sibu Dhital <cixwaresibu@gmail.com>
2020-03-01 12:21:44 +0545

    substring extraction with clear sentenced

--- string/substring.php
+++ string/substring.php
@@ -1,6 +1,6 @@
 <?php 
     $str = 'my name is donald trump';
-    echo substr($str,11,12);
+    echo 'the substring of the string '.$str.' is: '.substr($str,11,12);
     echo "<br>";
     echo $str[11];
 ?>
