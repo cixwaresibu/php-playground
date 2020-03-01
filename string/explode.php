87753dc4dae5d63161852aed20385ab2249a55cd
Sibu Dhital <cixwaresibu@gmail.com>
2020-03-01 15:21:31 +0545

    implode explode function example

--- string/explode.php
+++ string/explode.php
@@ -1,5 +1,9 @@
 <?php
     $str='hey you! how are you doin myan';
     print_r(explode(' ',$str,4));
+    echo "<br>";
     //explode('separator',string-variable,limit);
+
+    $array = ['sibu','dhital','is','a','bad','boy'];
+    echo implode(' ',$array).".";
 ?>
