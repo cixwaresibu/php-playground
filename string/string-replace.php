1b82765cad5ccea7c9ca0bfefae14fe6be4576af
Sibu Dhital <cixwaresibu@gmail.com>
2020-03-01 12:50:03 +0545

    replacing all matching substring from given string set with same another substring

---
+++ string/string-replace.php
@@ -0,0 +1,11 @@
+<?php
+    $str = 'hey! we are very obident student. we are student of csit and also we are student of php in cixware';
+    echo $str;
+    echo "<br>";
+    echo str_replace("student","teacher",$str);
+    $str1 = str_replace("student","teacher",$str);
+    echo "<br>";
+    echo str_replace("are","were",$str);
+    echo "<br>";
+    echo str_replace("are","were",$str1);
+?>
