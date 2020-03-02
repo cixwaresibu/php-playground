f9dc5f18c52dc11cbf332cb97425b7fd54b001ef
Sibu Dhital <cixwaresibu@gmail.com>
2020-03-02 15:48:54 +0545

    last occurance of a character or string

---
+++ string/strrpos.php
@@ -0,0 +1,5 @@
+<?php
+    $str = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, sapiente!';
+    echo strrpos($str,'R')."<br>"; //case sensitive
+    echo strripos($str,'SAP'); //case insensitive
+    ?>
