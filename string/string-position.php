65948aaf9bd229e5f6c3349b283c59b32b9b3474
Sibu Dhital <cixwaresibu@gmail.com>
2020-03-02 15:33:23 +0545

    understanding strpos stripos and strrpos

--- string/string-position.php
+++ string/string-position.php
@@ -2,5 +2,9 @@
     $str='hey it\'s me sakar and it is very important';
     echo $str;
     echo "<br>";
-    echo 'the given substring is at position ' . strpos($str,"it") .' in given string: '.'"' .$str. '"';
+    echo 'the given substring is at position ' . strpos($str,"IT") .' in given string: '.'"' .$str. '"';
+    echo "<br>";
+    echo 'the given substring is at position ' . stripos($str,"it") .' in given string: '.'"' .$str. '"';
+    echo "<br>";
+    echo 'the given substring is at position ' . strrpos($str,"it") .' in given string: '.'"' .$str. '"';
 ?>
