5f2bf5f8e4c654033dd4a7585bd5cab57fd34975
Sibu Dhital <cixwaresibu@gmail.com>
2020-03-06 11:41:48 +0545

    array_push function in php

---
+++ array/array_push.php
@@ -0,0 +1,8 @@
+<?php
+    $array = array(4,5,6);
+    print_r($array);
+    echo "<br>";
+    //array_push assign gives elements at the end without modifying other elements
+    array_push($array,7,8,9);
+    print_r($array);
+?>
