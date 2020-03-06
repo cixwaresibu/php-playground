d66ab35a84f9c6d30b9edb3ca49d8232feaf32ec
Sibu Dhital <cixwaresibu@gmail.com>
2020-03-06 11:55:21 +0545

    unset function in php

--- array/unset.php
+++ array/unset.php
@@ -1,5 +1,7 @@
 <?php
     $fruit = array("bananas", "apples", "peaches");
+    print_r($fruit);
+    echo "<br>";
     unset($fruit[1]);
     print_r($fruit);
     //unset function delete the element from given index
