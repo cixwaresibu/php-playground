edbd785ee84936920b5c0007dca1b5b9ef4b41f2
Sibu Dhital <cixwaresibu@gmail.com>
2020-03-06 12:34:06 +0545

    reverse sorting of array with arsort and krsort

--- array/arsort-krsort.php
+++ array/arsort-krsort.php
@@ -8,7 +8,7 @@
 ?>
 
 <?php
-    $fruit = array("bananas", "apples", "peaches","mango","orange","graps");
+    $fruit = array("f"=>"bananas","h"=> "apples","a"=> "peaches","i"=>"mango","b"=>"orange","c"=>"graps");
     print_r($fruit);
     echo "<br>";
     krsort($fruit); //krsort function sort the array in descending order considering the key  and also preserve the element associated with each key
@@ -24,13 +24,4 @@
     arsort($array);  //arsort function sort the array in descending order and also preserve the index
     print_r($array);
     echo "<br>";
-?>
-
-<?php 
-    $array = array(1,2,5,7,3,4,8,9,6);
-    print_r($array);
-    echo "<br>";
-    krsort($array); //krsort function sort the array in descending order considering the key  and also preserve the element associated with each key
-    print_r($array);
-    
 ?>
