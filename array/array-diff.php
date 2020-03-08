2629aa6578caf1ee04e2f41f1dd84c70aff32f6f
Sibu Dhital <cixwaresibu@gmail.com>
2020-03-08 11:53:08 +0545

    array_diff function of php

---
+++ array/array-diff.php
@@ -0,0 +1,8 @@
+<?php
+$a1=array("a"=>"red","b"=>"green","c"=>"blue","d"=>"yellow");
+$a2=array("e"=>"red","f"=>"black","g"=>"purple");
+$a3=array("a"=>"red","b"=>"black","h"=>"yellow");
+
+$result=array_diff($a1,$a2,$a3); //returns the value in $a1 which are not present in other array
+print_r($result);
+?>
